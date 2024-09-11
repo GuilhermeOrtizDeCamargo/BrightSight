d66eb8b280ead3f085970bc31df2f132c6bf44ea
DinizSptech <mateus.leite@sptech.scho>
2024-09-10 23:36:10 -0300

    Conclusão da tabela MySQL e inserção de dados

--- Tabelas Sprint 1.sql
+++ Tabelas Sprint 1.sql
@@ -14,18 +14,16 @@ númeroTelefonico char(11) NOT NULL
 
 CREATE TABLE DadosSensor(
 idDado int primary key auto_increment,
-DataDado date,
-HorárioDado time,
+DiaEHora datetime,
 Luminosidade int
 );
 
 CREATE TABLE Região(
 idArduíno int primary key auto_increment,
 NomeRegião varchar(40),
-EspaçoM² int
+EspaçoM² int,
+Média int
 );
-
-DROP TABLE DadosCliente;
 
 INSERT INTO DadosCliente VALUES
 (default,'WorkHard', 02177090,399, 80985284000151,'WorkHard@gmail.com',11995247192),
@@ -33,14 +31,20 @@ INSERT INTO DadosCliente VALUES
 (default,'BigFarma',53404-325,1037,49045027000190,'FarmaBusiness@outlook.com',8194357622);
 
 INSERT INTO  DadosSensor VALUES
-(default,2024-09-02,12-09-38,957),
-(default,2024-09-02,17-33-49,761),
-(default,2024-09-02,20-53-22,126),
-(default,2024-09-07,07-25-11,517),
-(default,2024-09-07,15-01-18,843),
-(default,2024-09-09,04-03-49,217),
-(default,2024-90-09,13-43-49,916);
+(default,'2024-09-02 12:09:38',957),
+(default,'2024-09-02 17:33:49',761),
+(default,'2024-09-02 20:53:22',126),
+(default,'2024-09-07 07:25:11',517),
+(default,'2024-09-07 15:01:18',843),
+(default,'2024-09-09 04:03:49',217),
+(default,'2024-09-09 13:43:49',916);
 
-INSERT INTO Região VALUES()
+INSERT INTO Região VALUES
+(default,'Nordeste',200,697),
+(default,'Sudeste',300,721),
+(default,'Sul','250',873),
+(default,'Norte','120','315'),
+(default,'Leste','400','913');
+
 
 
